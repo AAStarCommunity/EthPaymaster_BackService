e1f0c6538a63cc83bbb520f5bdf4aec3dfb992c7
dylanyang <yl976931569@163.com>
2024-04-22 22:25:37 +0800

    optimize gas

--- common/network/ethereum_adaptable_executor_test.go
+++ common/network/ethereum_adaptable_executor_test.go
@@ -2,13 +2,29 @@ package network
 
 import (
 	"AAStarCommunity/EthPaymaster_BackService/common/types"
+	"AAStarCommunity/EthPaymaster_BackService/common/userop"
+	"AAStarCommunity/EthPaymaster_BackService/common/utils"
 	"AAStarCommunity/EthPaymaster_BackService/conf"
 	"context"
 	"testing"
 )
 
-func SimulateV06HandleOp() {
-
+func TestSimulateV06HandleOp(t *testing.T) {
+	sepoliaExector := GetEthereumExecutor(types.EthereumSepolia)
+	op, newErr := userop.NewUserOp(utils.GenerateMockUserOperation(), types.EntrypointV06)
+	if newErr != nil {
+		return
+	}
+	opValue := *op
+	strategy := conf.GetBasicStrategyConfig("Ethereum_Sepolia_v06_verifyPaymaster")
+	userOpV6 := opValue.(*userop.UserOperationV06)
+	simulataResult, err := sepoliaExector.SimulateV06HandleOp(userOpV6, strategy.GetEntryPointAddress())
+	if err != nil {
+		return
+	}
+	if simulataResult == nil {
+		return
+	}
 }
 func TestEthereumExecutorClientConnect(t *testing.T) {
 	conf.BasicStrategyInit()
